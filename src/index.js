f20bdfbf15e185f0e761214f53af5e89818cdc06
Alina Bovdyr <alinabovdyr92@gmail.com>
2020-11-14 12:53:20 +0200

    Добавляет разметку страницы, карточки и стили

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-import './styles.css';
+import './sass/styles.scss';
+import countryCardTpl from './templates/country-card.hbs';
+
+const refs = {
+    cardContainer: document.querySelector('.js-card-container')
+}
+
+fetch('https://restcountries.eu/rest/v2/name/france')
+    .then(response => {
+        return response.json();
+    })
+    .then(country => {
+        console.log(country);
+        const markup = countryCardTpl(country);
+        refs.cardContainer.innerHTML = markup;
+    })
+    .catch(error => {
+        console.log(error);
+    })
